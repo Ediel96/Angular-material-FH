a893958030854cf64d0d112052d408daa07a8589
DarkEdiel96 <39193308+Ediel96@users.noreply.github.com>
2022-01-21 18:13:30 -0500

    configuracion con error

--- angular-material-heroe/src/app/pruebas/index/index.component.ts
+++ angular-material-heroe/src/app/pruebas/index/index.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PruebaService } from '../services/prueba.service';
-import {Orden , Producto} from '../model/objOrden.interfaces';
+import { Orden, Product, Result } from '../model/objOrden.interfaces';
 
 
 @Component({
@@ -15,6 +15,9 @@ export class IndexComponent implements OnInit {
   orden : Orden[] = [];
   ordenActual : Orden[] = [];
   listado :Orden[] = []
+  resultList : Result[] = [];
+  result : Result[] =[];
+  
   // producto : Producto ;
 
   ngOnInit(): void {
@@ -25,17 +28,32 @@ export class IndexComponent implements OnInit {
 
     this.pruebaServ.getHeores().subscribe(
         (res) => {
-          this.orden = res;
+          // this.orden = res;
+          this.resultList = res;
 
           console.log(this.orden)
 
-          this.orden.filter((res, i )=> {
+          this.resultList.filter((res, i )=> {
 
             if(this.ordenActual.length == 0){
-              this.ordenActual.push(res);
+              
+              this.ordenActual[0].CONSECUTIVO;
+              this.ordenActual[0].ORDEN;
+              this.ordenActual[0].TIENDA;
+              this.ordenActual[0].NOMBRE_TIENDA;
+              this.ordenActual[0].PROVEEDOR;
+              this.ordenActual[0].NOMBRE_PROVEEDOR;
+              this.ordenActual[0].FECHA_CREA;
+              this.ordenActual[0].USUARIO;
+              this.ordenActual[0].PRODUCTO;
+
+              const product = this.orden.filter(result => res.ORDEN == result.ORDEN);
+              console.log('product: ', product);
+              this.ordenActual[0]. = product[0];
+
             }else{
               console.log('contador :', i);
-              // this.ordenActual = this.ordenActual.filter(res => this.orden[i].ORDEN != res.ORDEN);
+
               for(let j = 0; j < this.ordenActual.length; j++){
                 if(this.ordenActual[j].ORDEN != this.orden[i].ORDEN){
                   this.ordenActual.push(res);
@@ -43,10 +61,27 @@ export class IndexComponent implements OnInit {
               }
             }
 
-          })
+          }
+          )
           console.log(this.ordenActual);
     });
 
+  }
+
+
+  returnListproduct(pruducto : Product, listProduct : Result[], posicio : number):Product {
+    // const resultProduct = [];
+    let resulProduct = pruducto;
+
+    listProduct.filter((result, i) =>{
+      resulProduct.PLANTA = result.PLANTA;
+      resulProduct.SKU = result.SKU;
+      resulProduct.NOMBRE_SKU = result.NOMBRE_SKU;
+      resulProduct.CANTIDAD = result.CANTIDAD;
+      resulProduct.COMENTARIO = result.COMENTARIO;
+    })
+    
+    return resulProduct ;
   }
 
 }
--- angular-material-heroe/src/app/pruebas/model/objOrden.interfaces.ts
+++ angular-material-heroe/src/app/pruebas/model/objOrden.interfaces.ts
@@ -7,10 +7,27 @@ export interface Orden {
   NOMBRE_PROVEEDOR: string,
   FECHA_CREA: string,
   USUARIO: string,
-  PRODUCTO: Producto[]
+  PRODUCTO: Array<Product>
 }
 
-export interface Producto {
+export interface Product {
+  PLANTA: number,
+  SKU: string,
+  NOMBRE_SKU: string,
+  CANTIDAD: number,
+  COMENTARIO: string 
+}
+
+
+export interface Result {
+  CONSECUTIVO: number,
+  ORDEN: number,
+  TIENDA: number,
+  NOMBRE_TIENDA: string,
+  PROVEEDOR: string,
+  NOMBRE_PROVEEDOR: string,
+  FECHA_CREA: string,
+  USUARIO: string,
   PLANTA: number,
   SKU: string,
   NOMBRE_SKU: string,
