e695cb9b5af154792a6f1bc4427557dfa5b7da12
DarkEdiel96 <39193308+Ediel96@users.noreply.github.com>
2022-01-23 21:06:42 -0500

    mejoradno filtrar productos

--- angular-material-heroe/src/app/pruebas/index/index.component.ts
+++ angular-material-heroe/src/app/pruebas/index/index.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PruebaService } from '../services/prueba.service';
 import { Orden, Product, Result } from '../model/objOrden.interfaces';
+import { NgTemplateOutlet } from '@angular/common';
 
 
 @Component({
@@ -12,75 +13,161 @@ export class IndexComponent implements OnInit {
 
   constructor( private pruebaServ : PruebaService) { }
 
-  orden : Orden[] = [];
   ordenActual : Orden[] = [];
   listado :Orden[] = []
   resultList : Result[] = [];
   result : Result[] =[];
-  
+  returnProduct : Product[] =[];
+
+
+  orden : Orden = new Orden();
+  product : Product = new Product();
+
+
+
   // producto : Producto ;
 
   ngOnInit(): void {
 
-    let hash = {};
-
-
-
     this.pruebaServ.getHeores().subscribe(
         (res) => {
           // this.orden = res;
-          this.resultList = res;
 
-          console.log(this.orden)
+          let resultado = [];
+          let resultadoOrden  = res;
+          let ordenesUnicass = []
 
-          this.resultList.filter((res, i )=> {
+          let orden = {
+            CONSECUTIVO : null,
+            ORDEN : null,
+            TIENDA : '',
+            NOMBRE_TIENDA : '',
+            PROVEEDOR :'',
+            NOMBRE_PROVEEDOR :'',
+            FECHA_CREA:'',
+            USUARIO:'',
+            PRODUCTO:[{PLANTA : null  , SKU: '', NOMBRE_SKU:'' , CANTIDAD: null, COMENTARIO:''}]
+          }
+
+          let producto = {PLANTA : null  , SKU: '', NOMBRE_SKU:'' , CANTIDAD: null, COMENTARIO:''}
+
+          let listaPorducto = [];
+
+          let ordenString = 0;
+          resultadoOrden.forEach((res, i ) => {
+            if(ordenString != res.ORDEN){
+              ordenString = res.ORDEN;
+              ordenesUnicass.push(res)
+            }
+          });
+
+          // console.log('ordenesUnicas', ordenesUnicass)
+
+          for (let i = 0; i < ordenesUnicass.length; i++){
+
+            if(resultado.length == 0){
+
+            }
+              const {CONSECUTIVO, ORDEN, TIENDA , NOMBRE_TIENDA, PROVEEDOR , NOMBRE_PROVEEDOR, FECHA_CREA, USUARIO} = resultadoOrden[0];
+
+              orden.CONSECUTIVO = CONSECUTIVO;
+              orden.ORDEN = ORDEN;
+              orden.TIENDA = TIENDA;
+              orden.NOMBRE_TIENDA = NOMBRE_TIENDA;;
+              orden.PROVEEDOR = PROVEEDOR;;
+              orden.NOMBRE_PROVEEDOR = NOMBRE_PROVEEDOR;;
+              orden.FECHA_CREA = FECHA_CREA;;
+              orden.USUARIO = USUARIO;
+
+              resultado.push(orden);
+
+              let productofiltrar = resultadoOrden.filter(res => orden.ORDEN == res.ORDEN);
+              for (let i2 = 0; i2 < productofiltrar.length; i2++){
+
+                producto = {PLANTA : null  , SKU: '', NOMBRE_SKU:'' , CANTIDAD: null, COMENTARIO:''}
+
+                let {PLANTA, SKU, NOMBRE_SKU , CANTIDAD, COMENTARIO } = resultadoOrden[i2];
+
+                producto.PLANTA = PLANTA;
+                producto.SKU = SKU;
+                producto.NOMBRE_SKU = NOMBRE_SKU;
+                producto.CANTIDAD = CANTIDAD;
+                producto.COMENTARIO = COMENTARIO;
+
+                listaPorducto.push(producto);
+
+              }
+              resultado[i].PRODUCTO = listaPorducto;
+
+
+          }
+
+          console.log(resultado);
+
+          // console.log(this.orden);
+
+        /*
+          this.resultList.map((res, i )=> {
 
             if(this.ordenActual.length == 0){
-              
-              this.ordenActual[0].CONSECUTIVO;
-              this.ordenActual[0].ORDEN;
-              this.ordenActual[0].TIENDA;
-              this.ordenActual[0].NOMBRE_TIENDA;
-              this.ordenActual[0].PROVEEDOR;
-              this.ordenActual[0].NOMBRE_PROVEEDOR;
-              this.ordenActual[0].FECHA_CREA;
-              this.ordenActual[0].USUARIO;
-              this.ordenActual[0].PRODUCTO;
+              let ordenActual : Orden = new Orden();
 
-              const product = this.orden.filter(result => res.ORDEN == result.ORDEN);
-              console.log('product: ', product);
-              this.ordenActual[0]. = product[0];
+              console.log('CONSECUTIVO: ',res);
+
+              this.orden.CONSECUTIVO = res.CONSECUTIVO;
+              this.orden.ORDEN = res.ORDEN;;
+              this.orden.TIENDA = res.TIENDA;;
+              this.orden.NOMBRE_TIENDA = res.NOMBRE_TIENDA;;
+              this.orden.PROVEEDOR = res.PROVEEDOR;;
+              this.orden.NOMBRE_PROVEEDOR = res.NOMBRE_PROVEEDOR;;
+              this.orden.FECHA_CREA = res.FECHA_CREA;;
+              this.orden.USUARIO = res.USUARIO;
+
+              this.orden = resultadoOrden[0];
+
+              console.log('ordenActual:', ordenActual);
+              // this.orden.push(this.orden)
+
+
+              // this.ordenActual[0].PRODUCTO;
+
+              // const product = this.orden.filter(result => {
+              //   res.ORDEN == result.ORDEN
+              // });
+
+              this.ordenActual[0].PRODUCTO = this.returnListproduct(this.returnProduct ,this.resultList);
 
             }else{
               console.log('contador :', i);
 
               for(let j = 0; j < this.ordenActual.length; j++){
-                if(this.ordenActual[j].ORDEN != this.orden[i].ORDEN){
-                  this.ordenActual.push(res);
-                }
+                // if(this.ordenActual[j].ORDEN != this.orden[i].ORDEN){
+                //   // this.ordenActual.push(res);
+                // }
               }
             }
-
           }
           )
-          console.log(this.ordenActual);
+          */
+
     });
 
   }
 
 
-  returnListproduct(pruducto : Product, listProduct : Result[], posicio : number):Product {
+  returnListproduct(pruducto : Product[], listProduct : Result[]):Product[] {
     // const resultProduct = [];
     let resulProduct = pruducto;
 
+
     listProduct.filter((result, i) =>{
-      resulProduct.PLANTA = result.PLANTA;
-      resulProduct.SKU = result.SKU;
-      resulProduct.NOMBRE_SKU = result.NOMBRE_SKU;
-      resulProduct.CANTIDAD = result.CANTIDAD;
-      resulProduct.COMENTARIO = result.COMENTARIO;
+      resulProduct[i].PLANTA = result.PLANTA;
+      resulProduct[i].SKU = result.SKU;
+      resulProduct[i].NOMBRE_SKU = result.NOMBRE_SKU;
+      resulProduct[i].CANTIDAD = result.CANTIDAD;
+      resulProduct[i].COMENTARIO = result.COMENTARIO;
     })
-    
+
     return resulProduct ;
   }
 
--- angular-material-heroe/src/app/pruebas/model/objOrden.interfaces.ts
+++ angular-material-heroe/src/app/pruebas/model/objOrden.interfaces.ts
@@ -1,36 +1,75 @@
-export interface Orden {
-  CONSECUTIVO: number,
-  ORDEN: number,
-  TIENDA: number,
-  NOMBRE_TIENDA: string,
-  PROVEEDOR: string,
-  NOMBRE_PROVEEDOR: string,
-  FECHA_CREA: string,
-  USUARIO: string,
+export class Orden {
+  CONSECUTIVO: number;
+  ORDEN: number;
+  TIENDA: number;
+  NOMBRE_TIENDA: string;
+  PROVEEDOR: string;
+  NOMBRE_PROVEEDOR: string;
+  FECHA_CREA: string;
+  USUARIO: string;
   PRODUCTO: Array<Product>
+
+  public constructor(){
+    this.CONSECUTIVO = 0,
+    this.ORDEN = 0,
+    this.TIENDA = 0,
+    this.NOMBRE_TIENDA = '',
+    this.PROVEEDOR = '',
+    this.NOMBRE_PROVEEDOR = '',
+    this.FECHA_CREA = '',
+    this.USUARIO = '',
+    this.PRODUCTO = []
+  }
 }
 
-export interface Product {
-  PLANTA: number,
-  SKU: string,
-  NOMBRE_SKU: string,
-  CANTIDAD: number,
-  COMENTARIO: string 
-}
-
-
-export interface Result {
-  CONSECUTIVO: number,
-  ORDEN: number,
-  TIENDA: number,
-  NOMBRE_TIENDA: string,
-  PROVEEDOR: string,
-  NOMBRE_PROVEEDOR: string,
-  FECHA_CREA: string,
-  USUARIO: string,
-  PLANTA: number,
-  SKU: string,
-  NOMBRE_SKU: string,
-  CANTIDAD: number,
+export class Product {
+  PLANTA: number;
+  SKU: string;
+  NOMBRE_SKU: string;
+  CANTIDAD: number;
   COMENTARIO: string
+
+  public constructor(){
+    this.PLANTA = 0;
+    this.SKU = '';
+    this.NOMBRE_SKU = '',
+    this.CANTIDAD = 0,
+    this.COMENTARIO = ''
+  }
+
+}
+
+
+export class Result {
+
+  CONSECUTIVO: number;
+  ORDEN: number;
+  TIENDA: number;
+  NOMBRE_TIENDA: string;
+  PROVEEDOR: string;
+  NOMBRE_PROVEEDOR: string;
+  FECHA_CREA: string;
+  USUARIO: string;
+  PLANTA: number;
+  SKU: string;
+  NOMBRE_SKU: string;
+  CANTIDAD: number;
+  COMENTARIO: string;
+
+  public constructor(){
+    this.CONSECUTIVO = 0;
+    this.ORDEN = 0;
+    this.TIENDA = 0;
+    this.NOMBRE_TIENDA = '';
+    this.PROVEEDOR = '';
+    this.NOMBRE_PROVEEDOR = '';
+    this.FECHA_CREA = '';
+    this.USUARIO = '';
+    this.PLANTA = 0;
+    this.SKU = '';
+    this.NOMBRE_SKU = '';
+    this.CANTIDAD = 0;
+    this.COMENTARIO = '';
+  }
+
 }
